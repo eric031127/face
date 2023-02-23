c5ac601bf5bbeeea3fa6c607c86f5cb8266dfdc6
陳秉廉 <124891096+eric031127@users.noreply.github.com>
2023-02-24 00:27:15 +0800

    Update import kivy.py

--- import kivy.py
+++ import kivy.py
@@ -1,13 +1,12 @@
 import kivy
 import cv2
+import requests
 import numpy as np
+import os
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-import requests
+
 
 r = requests.get('https://raw.githubusercontent.com/kipr/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml')
 s = r.text
@@ -38,78 +37,108 @@ class button(App):
         boxlayout.add_widget(btu1)
         return boxlayout
     def callback(self,event):
-        detector = cv2.CascadeClassifier('C:/Users/cheri/Desktop/homework/APP/text.xml') 
-        recognition = cv2.face.LBPHFaceRecognizer_create()    
-        faces = []   
-        ids = []
-        print('camera...')                              
+        face_detector = cv2.CascadeClassifier('C:/Users/cheri/Desktop/homework/APP/text.xml')  
+        samples = []
+        labels = []                             
         cap = cv2.VideoCapture(0)                    
         if not cap.isOpened():
             print("Cannot open camera")
             exit()
         while True:
-            ret, img = cap.read()                     
+            ret, frame = cap.read()                     
             if not ret:
                 print("Cannot receive frame")
                 break
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
-            img_np = np.array(gray,'uint8')               
-            face = detector.detectMultiScale(gray)        
-            for(x,y,w,h) in face:
-                faces.append(img_np[y:y+h,x:x+w])         
-                ids.append(3)                             
-            cv2.imshow('oxxostudio', img)  
             
-            if  cv2.waitKey(100) == 27:           
-                break
- 
-        print('training...')  
-        i=int()
-        recognition.train(faces,np.array(ids))                            
-        recognition.save(f'C:/Users/cheri/Desktop/homework/APP/face{i}.yml')                           
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = face_detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_gray = cv2.resize(roi_gray, (200, 200))
+                samples.append(roi_gray)
+                labels.append(0)                             
+            cv2.imshow('0', frame)  
+            
+            if  cv2.waitKey(1) & 0xFF == 27:           
+                break 
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        recognizer.train(samples, np.array(labels))
+        model_path = 'model/trained_model.xml'
+        if not os.path.exists('model'):
+            os.mkdir('model')
+        recognizer.write(model_path)                           
         print('ok!')
-        i=i+1
-        if i>30:
-            i=0
         cv2.destroyAllWindows()
     def callback1(self,event):
-        recognition = cv2.face.LBPHFaceRecognizer_create()
-        
-        cascade_path = 'C:/Users/cheri/Desktop/homework/APP/text.xml'  
-        face_cascade = cv2.CascadeClassifier(cascade_path)        
-        print('wait...')
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        recognizer.read('model/trained_model.xml')
+        face_cascade = cv2.CascadeClassifier('C:/Users/cheri/Desktop/homework/APP/text.xml')        
         cap = cv2.VideoCapture(0)                                 
         if not cap.isOpened():
             print("Cannot open camera")
             exit()
         
+        
         while True:
-            i=0
-            recognition.read(f'C:/Users/cheri/Desktop/homework/APP/face{i}.yml')
-            ret, img = cap.read()
+            ret, frame = cap.read()
             if not ret:
-                print("Cannot receive frame")
                 break
-            img = cv2.resize(img,(540,300))             
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
-            faces = face_cascade.detectMultiScale(gray) 
-            for(x,y,w,h) in faces:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)            
-                idnum,confidence = recognition.predict(gray[y:y+h,x:x+w])  
-                if confidence < 60:
-                    text = "yes"
-                                         
-                else:
-                    text = '???'                                          
-                cv2.putText(img, text, (x,y-5),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            cv2.imshow('finish', img)
-            i=i+1
-            if i>30:
-                i=0
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_gray = cv2.resize(roi_gray, (200,200))
+
+                
+                id_, confidence = recognizer.predict(roi_gray)
+
+                
+                if confidence < 50:
+                    
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+                    
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    name = 'Unknown'
+                    if id_ == 0:
+                        name = 'User1'
+                    elif id_ == 1:
+                        name = 'User2'
+                    elif id_ == 2:
+                        name = 'User3'
+                    elif id_ == 3:
+                        name = 'User4'
+                    elif id_ == 4:
+                        name = 'User5'
+                    elif id_ == 5:
+                        name = 'User6'
+                    elif id_ == 6:
+                        name = 'User7'
+                    elif id_ == 7:
+                        name = 'User8'
+                    elif id_ == 8:
+                        name = 'User9'
+                    elif id_ == 9:
+                        name = 'User10'
+                    elif id_ == 10:
+                        name = 'User11'
+                    cv2.putText(frame, name, (x+5, y-5), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                else:
+                    
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
             
-            if cv2.waitKey(1) == 27:
-                break    
+            cv2.imshow('Face Recognition', frame)
+
+            
+            if cv2.waitKey(1) & 0xFF == 27:
+                break  
         cap.release()
         cv2.destroyAllWindows()
         
